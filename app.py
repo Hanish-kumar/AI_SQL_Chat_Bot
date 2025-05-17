66a59bed21395acce3cf406c3cbe9d469ac2bf5e
Hanish Kumar Vydhyala <67914105+Hanish-kumar@users.noreply.github.com>
2025-05-17 18:33:07 +0530

    app.py

--- app.py
+++ app.py
@@ -92,11 +92,11 @@ st.title("Chat with Database")
 with st.sidebar:
     st.subheader("Settings")
     st.write("This is chat application using Database. Connect to the Database and assist yourself")
-    st.text_input("Host", value="http://sql12.freesqldatabase.com/", key="Host")
-    st.text_input("Port", value="3306", key="Port")
-    st.text_input("User", value="sql12779393", key="User")
-    st.text_input("Password", type="password", value="vydhyala@123", key="Password")
-    st.text_input("Database", value="sql12779393", key="Database")
+    st.text_input("Host", value="http://sql12.freesqldatabase.com/")
+    st.text_input("Port", value="3306")
+    st.text_input("User", value="sql12779393")
+    st.text_input("Password", type="password", value="vydhyala@123")
+    st.text_input("Database", value="sql12779393")
     if st.button("Connect"):
         with st.spinner("Connecting the Database..."):
             try:
